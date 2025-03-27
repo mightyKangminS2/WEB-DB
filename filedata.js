cc01733f54d8eafa4a1a29a344a43d35203f23fe
mightyKangminS2 <s2.kys.yt@gmail.com>
2025-03-27 13:58:09 +0900

    Add files via upload

---
+++ filedata.js
@@ -0,0 +1,15 @@
+const fileStructure = {
+    "Documents": {
+        "\\n0049" : {
+            "oi-0041R": "page1.html"
+        },
+        "hm-006" : {
+            "hs-0622R": "page2.html"
+        }
+    },
+    "Projects": {
+        "소설 폴더": {
+            "세 번째 문서": "page3.html"
+        }
+    }
+};
